4550f9939923543596839d4737dd01776f38f609
Henry Britton <hbritton@haverford.edu>
2023-06-26 15:11:16 -0400

    adding action node

---
+++ ActionNode.py
@@ -0,0 +1,12 @@
+import math
+import random
+class ActionNode:
+    def __init__(self, inputs, action):
+        self.action = action
+        self.activation = math.tanh(sum(inputs))
+    
+    def execute(self):
+        if self.activation > 0:
+            if random.random() > self.activation:
+                return self.action
+
--- Brain.py
+++ Brain.py
@@ -1,7 +1,13 @@
+import ActionNode
+
+
+
+
 class Brain:
     def __init__(self, genome):
         
         self.genome = genome
+        self.move_node = ActionNode.ActionNode([1,2,3], 'move')
 
   
     def wire_brain(self):
