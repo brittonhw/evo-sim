8a611306546e6cf234240a291cac91f7257fcb5b
ebritt07 <44182357+ebritt07@users.noreply.github.com>
2023-07-24 21:59:42 -0500

    add colors and zones

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,56 +2,193 @@ import "./App.css";
 import List from "./components/List";
 import Grid from "./components/Grid";
 import EditableButton from "./components/EditableButton";
+import { useState } from "react";
+
+export const drawingControls = {
+  clear: 0,
+  barrier: 1,
+  radioactive: 2,
+  safezone: 3,
+};
+
+export const drawingControlText = new Map([
+  [drawingControls.clear, "clear"],
+  [drawingControls.barrier, "barrier"],
+  [drawingControls.radioactive, "radioactive zone"],
+  [drawingControls.safezone, "safe zone"],
+]);
+
+export const colorMap = new Map([
+  [drawingControls.clear, "white"],
+  [drawingControls.barrier, "black"],
+  [drawingControls.radioactive, "yellow"],
+  [drawingControls.safezone, "lightgreen"],
+]);
 
 function App() {
-  const controls = [
-    { label: "safe zone", id: "1" },
-    { label: "barrier", id: "2" },
-    { label: "radioactive zone", id: "3" },
-  ];
+  const [rows, setRows] = useState(64);
+  const [cols, setCols] = useState(64);
 
+  const [activeDrawingControl, setActiveDrawingControl] = useState(
+    drawingControls.clear
+  );
 
+  const [gridData, setGridata] = useState(
+    Array.from({ length: rows }, () => Array(cols).fill(0))
+  );
 
-  const gridData = [
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-  ];
+  const handleRowChange = (event: any) => {
+    let valueInteger: number = parseInt(event.target.value);
+    valueInteger = valueInteger > 0 ? valueInteger : 1;
+    if (valueInteger < rows) {
+      const updatedGrid = gridData.slice(0, valueInteger);
+      setGridata(updatedGrid);
+    } else {
+      const rowsToAdd = valueInteger - rows;
+      const newRows = new Array(rowsToAdd).fill(new Array(cols).fill(0));
+      const updatedGrid = [...gridData, ...newRows];
+      setGridata(updatedGrid);
+    }
+    setRows(valueInteger);
+  };
+
+  const handleColChange = (event: any) => {
+    let valueInteger: number = parseInt(event.target.value);
+    valueInteger = valueInteger > 0 ? valueInteger : 1;
+    if (valueInteger < cols) {
+      const updatedGrid = gridData.map((row) => row.slice(0, valueInteger));
+      setGridata(updatedGrid);
+    } else {
+      const colsToAdd = valueInteger - cols;
+      const updatedGrid = gridData.map((row) => [
+        ...row,
+        ...new Array(colsToAdd).fill(0),
+      ]);
+      setGridata(updatedGrid);
+    }
+    setCols(valueInteger);
+  };
+
+  const handleDrawingControlChange = (control: number) => {
+    setActiveDrawingControl((prevControl) =>
+      prevControl === control ? drawingControls.clear : control
+    );
+  };
+
+  const handleCellClick = (rowIdx: number, colIdx: number) => {
+    const updatedGrid = gridData.map((row) => [...row]);
+    updatedGrid[rowIdx][colIdx] = activeDrawingControl;
+    setGridata(updatedGrid);
+  };
+
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [startY, setStartY] = useState(0);
+  const [endX, setEndX] = useState(0);
+  const [endY, setEndY] = useState(0);
+
+  const handleMouseDown = (e: any) => {
+    setIsDrawing(true);
+    setStartX(e.clientX);
+    setStartY(e.clientY);
+    setEndX(e.clientX);
+    setEndY(e.clientY);
+  };
+
+  const handleMouseMove = (e: any) => {
+    if (isDrawing) {
+      setEndX(e.clientX);
+      setEndY(e.clientY);
+    }
+  };
+
+  const [drawStart, setDrawStart] = useState([0, 0]);
+
+  const handleMouseUp = () => {
+    setIsDrawing(false);
+  };
+
+  const handleMouseDownDraw = (rowIdx: number, colIdx: number) => {
+    console.log("mousedown: " + rowIdx.toString() + " " + colIdx.toString());
+    setDrawStart([rowIdx, colIdx]);
+  };
+
+  const handleMouseUpDraw = (rowIdx: number, colIdx: number) => {
+    console.log("mouseup: " + rowIdx.toString() + " " + colIdx.toString());
+
+    const minRow = Math.min(drawStart[0], rowIdx);
+    const maxRow = Math.max(drawStart[0], rowIdx);
+    const minCol = Math.min(drawStart[1], colIdx);
+    const maxCol = Math.max(drawStart[1], colIdx);
+
+    const updatedGrid = gridData.map((row) => [...row]);
+    for (let i = minRow; i <= maxRow; i++) {
+      for (let j = minCol; j <= maxCol; j++) {
+        updatedGrid[i][j] = activeDrawingControl;
+      }
+    }
+    setGridata(updatedGrid);
+  };
 
   return (
     <>
-      <div className="flex-container">
-        <div className="main-grid">
-          <Grid data={gridData}></Grid>
+      <div className="header">
+        <div style={{ display: "flex" }}>
+          <h2>evolution sim</h2>
+          <button className="env">{process.env.NODE_ENV}</button>
         </div>
-
+      </div>
+      <div className="flex-container">
+        <div
+          className="main-grid"
+          onMouseDown={handleMouseDown}
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUp}
+        >
+          <Grid
+            data={gridData}
+            handleCellClick={handleCellClick}
+            handleMouseDownDraw={handleMouseDownDraw}
+            handleMouseUpDraw={handleMouseUpDraw}
+          />
+          {isDrawing && (
+            <div
+              style={{
+                position: "absolute",
+                left: Math.min(startX, endX),
+                top: Math.min(startY, endY),
+                width: Math.abs(endX - startX),
+                height: Math.abs(endY - startY),
+                border: "2px dashed blue",
+                zIndex: 999,
+                pointerEvents: "none",
+              }}
+            />
+          )}
+        </div>
         <div className="properties-pane">
           <div>
-            <List groupTitle="drawing controls" items={controls}></List>
+            <List
+              groupTitle="drawing controls"
+              handleClick={handleDrawingControlChange}
+              activeDrawingControl={activeDrawingControl}
+              id={Array.from({ length: 4 }, (_, index) => index)}
+            />
             <ul>
-              <p><b>grid dimensions</b></p>
-              <EditableButton promptText = {"rows:"} initialText={"12"}></EditableButton>
-              <EditableButton promptText = {"cols:"} initialText={"15"}></EditableButton>
+              <p>
+                <b>grid dimensions</b>
+              </p>
+              <EditableButton
+                promptText={"rows:"}
+                value={rows}
+                handleTextChange={handleRowChange}
+              />
+              <EditableButton
+                promptText={"cols:"}
+                value={cols}
+                handleTextChange={handleColChange}
+              />
             </ul>
-
-            <ul>
-              <p><b>simulation properties</b></p>
-              <EditableButton promptText = {"# generations"} initialText={"1000"}></EditableButton>
-              <EditableButton promptText = {"population start"} initialText={"14"}></EditableButton>
-            </ul>
-            <br></br>
-
-            <List groupTitle="" items={[{ label: "Run Simulation", id: "1" }]}></List>
-            <List groupTitle="" items={[{ label: "Replay Simulation", id: "1" }]}></List>
-            
-
           </div>
         </div>
       </div>
