6d68d11c782e9f7c1b8ddf00781bcaff075d5114
ebritt07 <44182357+ebritt07@users.noreply.github.com>
2023-12-17 12:39:47 -0600

    add another test case

--- backend/src/test/utils/position_encoding/test_position_encoders.py
+++ backend/src/test/utils/position_encoding/test_position_encoders.py
@@ -1,4 +1,5 @@
 import random
+import pytest
 
 
 from src.main.utils.position_encoding.decoder \
@@ -10,11 +11,9 @@ from src.main.utils.position_encoding.encoder \
     import convert_animation_data_to_bytes
 
 
-def test_encode_and_decode():
-
-    population_size = 3500
+def get_animation_data(lifecycle_steps = 400) -> AnimationData:
     gameboard_size = 256
-    lifecycle_steps = 400
+    population_size = 3500
     creature_positions_list = []
 
     for i in range(population_size):
@@ -23,19 +22,37 @@ def test_encode_and_decode():
             0, gameboard_size
         ), random.randrange(0, gameboard_size)
         positions = [last_position]
-        for _ in range(lifecycle_steps - 1):
+        for _ in range(400 - 1):
             newest_position = move_random_direction(
                 positions[-1], gameboard_size)
             positions.append(newest_position)
         creature_positions_dto.position_data = positions
         creature_positions_list.append(creature_positions_dto)
 
-    animation_data = AnimationData(
+    return AnimationData(
         steps=lifecycle_steps, creature_positions=creature_positions_list
     )
 
-    data_bytes = convert_animation_data_to_bytes(animation_data)
 
+def test_encode_and_decode():
+
+    animation_data = get_animation_data()
+    data_bytes = convert_animation_data_to_bytes(animation_data)
     result_animation_data = convert_bytes_to_animation_dto(data_bytes)
 
     assert result_animation_data.steps == animation_data.steps
+
+
+def test_encode_with_too_many_steps_raises_exception():
+    animation_data = get_animation_data(lifecycle_steps=400000)
+
+    try:
+        convert_animation_data_to_bytes(animation_data)
+        raise RuntimeError("this function should have thrown an error\
+                            under test, but it did not.")
+    except Exception as ex:
+        assert str(ex) == 'not enough bytes to represent steps!'
+
+
+
+
